82f3ffc409c810a860f2ffff43af75a47de00c19
Your Name <you@example.com>
2023-07-15 06:28:33 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,15 +13,15 @@ int main(void)
 		for (ones = '0'; ones <= '9'; ones++)
 		{
 			if (!((ones == tens) || (tens > ones)))
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && tens == '8'))
 				{
-					putchar(tens);
-					putchar(ones);
-					if (!(ones == '9' && tens == '8'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
+			}
 		}
 	}
 	putchar('\n');
